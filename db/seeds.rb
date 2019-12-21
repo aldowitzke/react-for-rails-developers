9b82ea57b76d004366b3886dd5b31e92aa4a010f
Aldo Witzke <aldo_mwa@hotmail.com>
2019-12-21 20:20:41 -0300

    pulls info from json

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ course = Course.create(title: 'Hello world', description: 'Create a react app wi
 section = Section.create(title: 'Chapter 1', course: course)
 
 episodes = Episode.create([
-  {title: '1. Setting up a new Ruby on Rails App with React..', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section_id: section.id},
-  {title: '2. Adding React to an Existing Rails App.', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section_id: section.id},
-  {title: '3. Building a Hello World App.', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section_id: section.id},
-  {title: '4. Adding React Router Dom to your App.', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section_id: section.id}
+  {title: '1. Setting up a new Ruby on Rails App with React..', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section},
+  {title: '2. Adding React to an Existing Rails App.', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section},
+  {title: '3. Building a Hello World App.', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section},
+  {title: '4. Adding React Router Dom to your App.', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section}
 ])
